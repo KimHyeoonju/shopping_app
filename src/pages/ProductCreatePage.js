35780a2aedc5ace74429f644c908bd99ffbd4855
KimHyeoonju <tttymttst@gmail.com>
2024-05-17 09:40:55 +0900

    docs : 백업

---
+++ src/pages/ProductCreatePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductCreateForm from "../components/create/ProductCreateForm";
+
+const ProductCreatePage = () => {
+  return (
+    <div>
+      <h2>상품 등록 페이지입니다.</h2>
+      <ProductCreateForm />
+    </div>
+  );
+};
+
+export default ProductCreatePage;
